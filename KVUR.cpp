29c24036e32a3b8597af6037c6ac2cc6357df5dd
PavelTarakanov <t.p.d.23102007@mail.ru>
2025-08-23 18:47:11 +0300

    Made consts and equation_solver

--- KVUR.cpp
+++ KVUR.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
-#include <TXLib.h>
 #include <math.h>
+#include <assert.h>
 
-int ckeck_null(double root);
+int check_null(double root);
 
+int equation_solver(double coefficient_a, double coefficient_b, double coefficient_c,
+                    double* root_1,       double* root_2);
 int square_solver(double coefficient_a, double coefficient_b, double coefficient_c,
                   double* root_1,       double* root_2);
 
@@ -11,13 +13,13 @@ int linear_solver(double coefficient_b, double coefficient_c, double* root_1);
 
 double diskr_calculate(double coefficient_a, double coefficient_b, double coefficient_c);
 
-int input(double* coefficient_a, double* coefficient_b, double* coefficient_c);
+int input_coefficients(double* coefficient_a, double* coefficient_b, double* coefficient_c);
 double scanf_check(double* coefficient_a);
 void cleaner(void);
 
 int finish(bool* more);
 
-int output(int root_count,double root_1,double root_2);
+int output_roots(int root_count,double root_1,double root_2);
 
 void test_square_solver();
 
@@ -28,6 +30,14 @@ void one_test_square_solver(
 
 void root_changer(double* root_1, double* root_2);
 
+enum Number_of_Roots
+{
+NO_ROOTS = 0,
+ONE_ROOT = 1,
+TWO_ROOTS = 2,
+INF_ROOTS = -1
+};
+const double INACCURACY = 0.0001;
 int main(void)
 {
     double coefficient_a = NAN, coefficient_b = NAN, coefficient_c = NAN;
@@ -38,11 +48,11 @@ int main(void)
 
     while (is_continue)
     {
-        input(&coefficient_a, &coefficient_b, &coefficient_c);
+        input_coefficients(&coefficient_a, &coefficient_b, &coefficient_c);
 
-        int root_count = square_solver(coefficient_a, coefficient_b, coefficient_c, &root_1, &root_2);
+        int root_count = equation_solver(coefficient_a, coefficient_b, coefficient_c, &root_1, &root_2);
 
-        output(root_count, root_1, root_2);
+        output_roots(root_count, root_1, root_2);
 
         finish(&is_continue);
     }
@@ -50,7 +60,7 @@ int main(void)
 
 }
 
-int input(double* coefficient_a, double* coefficient_b, double* coefficient_c)
+int input_coefficients(double* coefficient_a, double* coefficient_b, double* coefficient_c)
 {
     assert (coefficient_a);
     assert (coefficient_b);
@@ -60,13 +70,13 @@ int input(double* coefficient_a, double* coefficient_b, double* coefficient_c)
     assert (coefficient_b != coefficient_c);
     assert (coefficient_a != coefficient_c);
 
-    printf("Введите коэффицент при X^2: ");
+    printf("Input X^2 coefficient: ");
     scanf_check(coefficient_a);
 
-    printf("Введите коэффицент при Х: ");
+    printf("Input X coefficient: ");
     scanf_check(coefficient_b);
 
-    printf("Введите свободный член: ");
+    printf("Input free coefficient: ");
     scanf_check(coefficient_c);
 
     return 0;
@@ -76,11 +86,11 @@ double scanf_check(double* coefficient_a)
  {
     assert (coefficient_a);
 
-    int is_number = NAN;
+    int is_number = -1;
 
     while ((is_number = scanf("%lf", coefficient_a)) != 1)
     {
-        printf("Введите число!\n");
+        printf("Input number!\n");
 
         cleaner();
     }
@@ -94,56 +104,63 @@ void cleaner(void)
             continue;
         }
 }
-int square_solver(double  coefficient_a,  double  coefficient_b, double coefficient_c,
-                  double* root_1, double* root_2)
-
+int equation_solver(double coefficient_a, double coefficient_b, double coefficient_c,
+                     double* root_1,       double* root_2)
 {
-    assert(root_1);
-    assert(root_2);
-    assert(root_1 != root_2);
-
-    if (ckeck_null(coefficient_a))
+    if (check_null(coefficient_a))
     {
         return linear_solver(coefficient_b, coefficient_c, root_1);
     }
     else
     {
-        double diskr = diskr_calculate(coefficient_a, coefficient_b, coefficient_c);
-
-        if (diskr < 0)
-        {
-            return 0;
-        }
-        else if (ckeck_null(diskr))
-        {
-            *root_1 = -coefficient_b/(2*coefficient_a);
-
-            if (ckeck_null(*root_1))
-            {
-                *root_1 = 0;
-            }
-
-            return 1;
-        }
-        else
-        {
-            double sqrt_diskr = sqrt(diskr);
-
-            *root_1 = (-coefficient_b+sqrt_diskr)/(2*coefficient_a);
-            *root_2 = (-coefficient_b-sqrt_diskr)/(2*coefficient_a);
-
-            if (ckeck_null(*root_1))
-            {
-                *root_1 = 0;
-            }
-            if (ckeck_null(*root_2))
-            {
-                *root_2 = 0;
-            }
-            return 2;
-        }
-
+        return square_solver(coefficient_a, coefficient_b, coefficient_c,
+                             root_1, root_2);
     }
+}
+int square_solver(double  coefficient_a,  double  coefficient_b, double coefficient_c,
+                  double* root_1, double* root_2)
+
+{
+    assert (root_1);
+    assert (root_2);
+    assert (root_1 != root_2);
+
+
+    double diskr = diskr_calculate(coefficient_a, coefficient_b, coefficient_c);
+
+    if (diskr < 0)
+    {
+        return NO_ROOTS;
+    }
+    else if (check_null(diskr))
+    {
+        *root_1 = -coefficient_b/(2*coefficient_a);
+
+        if (check_null(*root_1))
+        {
+            *root_1 = 0;
+        }
+
+        return ONE_ROOT;
+    }
+    else
+    {
+        double sqrt_diskr = sqrt(diskr);
+
+        *root_1 = (-coefficient_b+sqrt_diskr)/(2*coefficient_a);
+        *root_2 = (-coefficient_b-sqrt_diskr)/(2*coefficient_a);
+
+        if (check_null(*root_1))
+        {
+            *root_1 = 0;
+        }
+        if (check_null(*root_2))
+        {
+            *root_2 = 0;
+        }
+        return TWO_ROOTS;
+    }
+
 }
 
 void test_square_solver()
@@ -163,7 +180,7 @@ void one_test_square_solver(double coefficient_a, double coefficient_b, double c
     assert (root_2);
     assert (root_1 != root_2);
 
-    int nRoots =  square_solver(coefficient_a, coefficient_b, coefficient_c, root_1, root_2);
+    int nRoots =  equation_solver(coefficient_a, coefficient_b, coefficient_c, root_1, root_2);
 
     if (nRoots == 2)
     {
@@ -172,9 +189,9 @@ void one_test_square_solver(double coefficient_a, double coefficient_b, double c
 
         if (!(nRoots == true_nRoots && *root_1 == true_root_1 && *root_2 == true_root_2))
         {
-            printf("Произошла ошибка: square_solver(%lf, %lf, %lf, ...), nRoots == %d, root_1 = %lf, root_2 = %lf\n",
+            printf("ERROR: square_solver(%lf, %lf, %lf, ...), nRoots == %d, root_1 = %lf, root_2 = %lf\n",
                     coefficient_a, coefficient_b, coefficient_c, nRoots, *root_1, *root_2);
-            printf("Правильный ответ: nRoots = %d, root_1 = %lf, root_2 = %lf\n",
+            printf("Correct answer: nRoots = %d, root_1 = %lf, root_2 = %lf\n",
                     true_nRoots, true_root_1, true_root_2);
         }
     }
@@ -183,9 +200,9 @@ void one_test_square_solver(double coefficient_a, double coefficient_b, double c
     {
         if (!(nRoots == true_nRoots && *root_1 == true_root_1))
         {
-            printf("Произошла ошибка: square_solver(%lf, %lf, %lf, ...), nRoots = %d, root_1 = %lf\n",
+            printf("ERROR: square_solver(%lf, %lf, %lf, ...), nRoots = %d, root_1 = %lf\n",
                     coefficient_a, coefficient_b, coefficient_c, nRoots, *root_1);
-            printf("Правильный ответ: nRoots = %d, root = %lf\n",
+            printf("Correct answer: nRoots = %d, root = %lf\n",
                     true_nRoots, true_root_1);
         }
     }
@@ -193,9 +210,9 @@ void one_test_square_solver(double coefficient_a, double coefficient_b, double c
     {
         if (nRoots != true_nRoots)
         {
-        printf("Произошла ошибка: square_solver(%lf, %lf, %lf, ...), nRoots = %d\n",
+        printf("ERROR: square_solver(%lf, %lf, %lf, ...), nRoots = %d\n",
                     coefficient_a, coefficient_b, coefficient_c, nRoots);
-        printf("Правильный ответ: nRoots = %d\n",
+        printf("Correct answer: nRoots = %d\n",
                     true_nRoots);
         }
     }
@@ -216,9 +233,9 @@ void root_changer(double *root_1, double *root_2)
     }
 }
 
-int ckeck_null(double root)
+int check_null(double root)
 {
-    return fabs(root) < 0.0001;
+    return fabs(root) < INACCURACY;
 }
 
 int linear_solver(double coefficient_b, double coefficient_c,
@@ -226,22 +243,22 @@ int linear_solver(double coefficient_b, double coefficient_c,
 {
     assert (root_1);
 
-    if (ckeck_null(coefficient_b))
+    if (check_null(coefficient_b))
     {
-        if (ckeck_null(coefficient_c))
+        if (check_null(coefficient_c))
         {
-            return -1;
+            return INF_ROOTS;
         }
         else
         {
-            return 0;
+            return NO_ROOTS;
         }
     }
     else
     {
         *root_1 = -coefficient_c/coefficient_b;
 
-        return 1;
+        return ONE_ROOT;
     }
 }
 
@@ -250,19 +267,19 @@ double diskr_calculate(double coefficient_a, double coefficient_b, double coeffi
     return (coefficient_b*coefficient_b-4*coefficient_a*coefficient_c);
 }
 
-int output(int root_count, double root_1, double root_2)
+int output_roots(int root_count, double root_1, double root_2)
 {
     switch(root_count)
     {
-        case 0: printf("Нет действительных корней\n");
+        case NO_ROOTS: printf("Equation has no roots\n");
+                        break;
+        case ONE_ROOT: printf("Answer: x = %lf\n", root_1);
+                        break;
+        case TWO_ROOTS: printf("Answer: x1 = %lf , x2 = %lf\n", root_1, root_2);
+                        break;
+        case INF_ROOTS: printf("Equation is always correct(all x are roots\n");
                 break;
-        case 1: printf("Ответ: х = %lf\n", root_1);
-                break;
-        case 2: printf("Ответ: х1 = %lf , х2 = %lf\n", root_1, root_2);
-                break;
-        case -1: printf("Уравнение верно при любых х\n");
-                break;
-        default: printf("Ошибка!");
+        default: printf("Mistake!");
                 break;
     }
 
@@ -275,8 +292,13 @@ int finish(bool* is_continue)
 
     int m_or_s = 0;
 
+    printf("If you want to continue input m\n");
+    printf("If you want to finish programm input s\n");
+
     while (1)
     {
+        cleaner();
+
         m_or_s = getchar();
 
         if (m_or_s == 's')
@@ -291,8 +313,8 @@ int finish(bool* is_continue)
         }
         else
         {
-            printf("Для продолжения работы введите m\n");
-            printf("Для завершения работы введите s\n");
+            printf("If you want to continue input m\n");
+            printf("If you want to finish programm input s\n");
 
             continue;
         }
