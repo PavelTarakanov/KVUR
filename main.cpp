6c4b601f20ea8860708239bc6165ccb5bdcaf54d
PavelTarakanov <t.p.d.23102007@mail.ru>
2025-08-25 19:38:41 +0300

    Used struct

--- main.cpp
+++ main.cpp
@@ -5,19 +5,20 @@
 
 int main(void)
 {
-    double coefficients [3] = {NAN, NAN, NAN};
-    double roots[2] = {NAN, NAN};
     bool is_continue = true;
+    SquareEquationData test = {.coefficient_a = NAN, .coefficient_b = NAN, .coefficient_c = NAN,
+                     .root_1 = NAN, .root_2 = NAN,
+                     .nRoots = 3};
 
     test_square_solver();
 
     while (is_continue)
     {
-        input_coefficients(coefficients);
+        input_coefficients(&test);
 
-        int root_count = equation_solver(coefficients, roots);
+        int root_count = equation_solver(&test);
 
-        output_roots(root_count, roots);
+        output_roots(root_count, &test);
 
         finish_program(&is_continue);
     }
@@ -25,82 +26,6 @@ int main(void)
     return 0;
 }
 
-void test_square_solver()
-{
-    double roots[2] = {NAN, NAN};
-
-    double coefficients_1[3] = {1, -5, 6};
-    one_test_square_solver(coefficients_1,  2,  3, 2, roots);
-    double coefficients_2[3] = {1, -2, 1};
-    one_test_square_solver(coefficients_2,  1,  0, 1, roots);
-    double coefficients_3[3] = {0, 0, 1};
-    one_test_square_solver(coefficients_3,  0,  0, 0, roots);
-}
-
-void one_test_square_solver(double coefficients[3],
-                            double true_root_1,   double true_root_2,   int true_nRoots,
-                            double roots[2])
-{
-    assert (!check_null(roots[0]));
-    assert (!check_null(roots[1]));
-    assert (!double_comparison(roots[0], roots[1]));
-
-    int nRoots =  equation_solver(coefficients, roots);
-
-    if (nRoots == 2)
-    {
-        root_sorter(roots);
-
-        if (!(nRoots == true_nRoots && double_comparison(roots[0], true_root_1)
-            && double_comparison(roots[1], true_root_2)))
-        {
-            printf("ERROR: square_solver(%lf, %lf, %lf, ...), nRoots == %d, root_1 = %lf, root_2 = %lf\n",
-                    coefficients[0], coefficients[1],coefficients[2],
-                    nRoots, roots[0], roots[1]);
-            printf("Correct answer: nRoots = %d, root_1 = %lf, root_2 = %lf\n",
-                    true_nRoots, true_root_1, true_root_2);
-        }
-    }
-
-    if (nRoots == 1)
-    {
-        if (!(nRoots == true_nRoots && double_comparison(roots[0], true_root_1)))
-
-        {
-            printf("ERROR: square_solver(%lf, %lf, %lf, ...), nRoots == %d, root_1 = %lf, root_2 = %lf\n",
-                    coefficients[0], coefficients[1],coefficients[2],
-                    nRoots, roots[0], roots[1]);
-            printf("Correct answer: nRoots = %d, root_1 = %lf, root_2 = %lf\n",
-                    true_nRoots, true_root_1, true_root_2);
-        }
-    }
-    if (nRoots == 0)
-    {
-        if (nRoots != true_nRoots)
-        {
-        printf("ERROR: square_solver(%lf, %lf, %lf, ...), nRoots == %d, root_1 = %lf, root_2 = %lf\n",
-                    coefficients[0], coefficients[1],coefficients[2],
-                    nRoots, roots[0], roots[1]);
-        printf("Correct answer: nRoots = %d, root_1 = %lf, root_2 = %lf\n",
-                    true_nRoots, true_root_1, true_root_2);
-        }
-    }
-}
-
-void root_sorter(double roots[2])
-{
-    assert (!check_null(roots[0]));
-    assert (!check_null(roots[1]));
-    assert (!double_comparison(roots[0], roots[1]));
-
-    double tmp = NAN;
-    if (roots[0] > roots[1])
-    {
-        tmp = roots[0];
-        roots[0] = roots[1];
-        roots[1] = tmp;
-    }
-}
 
 int finish_program(bool* is_continue)
 {
@@ -113,9 +38,8 @@ int finish_program(bool* is_continue)
 
     while (1)
     {
-        clean_buffer();
-
         want_solve_more = getchar();
+        clean_buffer();
 
         if (want_solve_more == 's')
         {
@@ -137,18 +61,4 @@ int finish_program(bool* is_continue)
     }
 
     return 0;
-}
-
-int double_comparison(double number_1, double number_2)
-{
-    double tmp = NAN;
-
-    if (number_1 > number_2)
-    {
-        tmp = number_1;
-        number_1 = number_2;
-        number_2 = tmp;
-    }
-
-    return (number_1 + INACCURACY > number_2);
 }
