2e755b73a32264f465ecbaf28f3d1be04a0c24ef
PavelTarakanov <t.p.d.23102007@mail.ru>
2025-08-25 19:38:57 +0300

    Used struct

--- output.cpp
+++ output.cpp
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include "square_solver.h"
 
-int output_roots(int root_count, double *roots)
+int output_roots(int root_count, SquareEquationData* test)
 {
+    assert(test);
+
     switch(root_count)
     {
         case NO_ROOTS: printf("Equation has no roots\n");
                         break;
-        case ONE_ROOT: printf("Answer: x = %lf\n", roots[0]);
+        case ONE_ROOT: printf("Answer: x = %lf\n", test->root_1);
                         break;
-        case TWO_ROOTS: printf("Answer: x1 = %lf , x2 = %lf\n", roots[0], roots[1]);
+        case TWO_ROOTS: printf("Answer: x1 = %lf , x2 = %lf\n", test->root_1, test->root_2);
                         break;
         case INF_ROOTS: printf("Equation is always correct(all x are roots\n");
                 break;
