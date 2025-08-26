30f785e9110b4bbec6cb11a25f976dc46a0211fc
PavelTarakanov <t.p.d.23102007@mail.ru>
2025-08-26 16:55:48 +0300

    Do tests using file input

--- tests.cpp
+++ tests.cpp
@@ -5,20 +5,47 @@
 
 void test_square_solver()
 {
-    SquareEquationData tests_coefficients[] =  {
-                        {.coefficient_a = 1, .coefficient_b = -5, .coefficient_c = 6,
-                         .root_1 = NAN, .root_2 = NAN, .nRoots = 2},
-                        {.coefficient_a = 1, .coefficient_b = -2, .coefficient_c = 1,
-                         .root_1 = NAN, .root_2 = NAN, .nRoots = 2}
-                        };
-    SquareEquationDataTests tests_answers[] = {
-                        {.correct_root_1 = 2, .correct_root_2 = 3,
-                         .correct_nRoots = 2},
-                        {.correct_root_1 = 1, .correct_root_2 = NAN,
-                         .correct_nRoots = 1}
-                        };
+    square_equation_data_coefficients tests_coefficients[10] =  {};
+    square_equation_data_correct tests_answers[10] = {};
 
+    FILE* fp;
+    fp = fopen("tests.txt", "r");
 
+    square_equation_data_coefficients file_input_coefficients = {
+            .coefficient_a = NAN, .coefficient_b = NAN, .coefficient_c = NAN,
+            .root_1 = NAN, .root_2 = NAN,
+            .nRoots = UNKNOWN_NUMBER_OF_ROOTS};
+
+    square_equation_data_correct file_input_answers = {
+            .correct_root_1 = NAN, .correct_root_2 = NAN,
+            .correct_nRoots = UNKNOWN_NUMBER_OF_ROOTS};
+
+    int tests_counter = 0;
+
+    while (fscanf(fp, "%lf %lf %lf %lf %lf %d %lf %lf %d",
+                  &file_input_coefficients.coefficient_a,
+                  &file_input_coefficients.coefficient_b,
+                  &file_input_coefficients.coefficient_c,
+                  &file_input_coefficients.root_1,
+                  &file_input_coefficients.root_2,
+                  &file_input_coefficients.nRoots,
+                  &file_input_answers.correct_root_1,
+                  &file_input_answers.correct_root_2,
+                  &file_input_answers.correct_nRoots) == 9)
+    {
+        tests_coefficients[tests_counter] = file_input_coefficients;
+        tests_answers[tests_counter] = file_input_answers;
+
+        tests_counter++;
+
+        if (getc(fp) = EOF)
+        {
+            break;
+        }
+
+    }
+
+    fclose (fp);
 
     size_t size = sizeof(tests_coefficients)/sizeof(tests_coefficients[0]);
 
@@ -28,8 +55,8 @@ void test_square_solver()
     }
 }
 
-void one_test_square_solver(SquareEquationData* test_coefficients,
-                            SquareEquationDataTests* test_answers)
+void one_test_square_solver(square_equation_data_coefficients* test_coefficients,
+                            square_equation_data_correct* test_answers)
 {
     assert (test_coefficients);
     assert (test_answers);
@@ -39,8 +66,8 @@ void one_test_square_solver(SquareEquationData* test_coefficients,
     root_sorter(test_coefficients);
 
     if (!(nRoots == test_answers->correct_nRoots
-        && double_comparison(test_coefficients->root_1, test_answers->correct_root_1)
-        && double_comparison(test_coefficients->root_2, test_answers->correct_root_2)))
+        && double_equal_comparison(test_coefficients->root_1, test_answers->correct_root_1)
+        && double_equal_comparison(test_coefficients->root_2, test_answers->correct_root_2)))
     {
         printf("ERROR: square_solver(%lf, %lf, %lf, ...), nRoots == %d, root_1 = %lf, root_2 = %lf\n"
                "Correct answer: nRoots = %d, root_1 = %lf, root_2 = %lf\n",
@@ -50,10 +77,9 @@ void one_test_square_solver(SquareEquationData* test_coefficients,
     }
 }
 
-void root_sorter(SquareEquationData* test)
+void root_sorter(square_equation_data_coefficients* test)
 {
     assert (test);
-
 
     double tmp = NAN;
     if (test->root_1 > test->root_2)
@@ -65,24 +91,15 @@ void root_sorter(SquareEquationData* test)
 }
 
 
-int double_comparison(double number_1, double number_2)
+int double_equal_comparison(double number_1, double number_2)
 {
-    double tmp = NAN;
-
-    if (isnan(number_1))
+    if (isnan(number_1) || isnan(number_2))
     {
-        if (isnan(number_2))
+        if (isnan(number_1) && isnan(number_2))
         {
             return 1;
         }
         return 0;
     }
-
-    if (number_1 > number_2)
-    {
-        tmp = number_1;
-        number_1 = number_2;
-        number_2 = tmp;
-    }
-    return (number_1 + INACCURACY > number_2);
+    return abs(number_1 - number_2) < INACCURACY;
 }
