acbd8ba5fb31ba73cd20761931129eb94a53ab47
PavelTarakanov <t.p.d.23102007@mail.ru>
2025-08-26 16:55:06 +0300

    Do tests using file input

--- output.cpp
+++ output.cpp
@@ -2,23 +2,26 @@
 #include <stdio.h>
 #include "square_solver.h"
 
-int output_roots(int root_count, SquareEquationData* test)
+void output_roots(int root_count, square_equation_data_coefficients* test)
 {
     assert(test);
 
     switch(root_count)
     {
-        case NO_ROOTS: printf("Equation has no roots\n");
-                        break;
-        case ONE_ROOT: printf("Answer: x = %lf\n", test->root_1);
-                        break;
-        case TWO_ROOTS: printf("Answer: x1 = %lf , x2 = %lf\n", test->root_1, test->root_2);
-                        break;
-        case INF_ROOTS: printf("Equation is always correct(all x are roots\n");
-                break;
-        default: printf("Mistake!");
+        case NO_ROOTS:
+            printf("Equation has no roots\n");
+            break;
+        case ONE_ROOT:
+            printf("Answer: x = %lf\n", test->root_1);
+            break;
+        case TWO_ROOTS:
+            printf("Answer: x1 = %lf , x2 = %lf\n", test->root_1, test->root_2);
+            break;
+        case INF_ROOTS:
+            printf("Equation is always correct(all x are roots\n");
+            break;
+        default: printf("Mistake!\n");
                 break;
     }
-
-    return 0;
+    return;
 }
