1baf52ef779e42f38121426cd964f18937ff805f
PavelTarakanov <t.p.d.23102007@mail.ru>
2025-08-28 14:19:32 +0300

    File input

--- square_solver.h
+++ square_solver.h
@@ -10,40 +10,47 @@ enum Number_of_Roots
     UNKNOWN_NUMBER_OF_ROOTS = 3
 };
 
-struct square_equation_data_coefficients {double coefficient_a, coefficient_b, coefficient_c;
+struct square_equation_data {double coefficient_a, coefficient_b, coefficient_c;
                             double root_1, root_2;
                             Number_of_Roots nRoots;};
 
-struct square_equation_data_correct {double correct_root_1, correct_root_2;
-                                   Number_of_Roots correct_nRoots;};
+struct reference_solutions_data {
+    double correct_root_1, correct_root_2;
+    Number_of_Roots correct_nRoots;
+};//TODO use struct for solutions and for coeff
+
+void Poltorashkas_greeting(void);
 
 int check_null(double root);
 
-int equation_solver(square_equation_data_coefficients* test);
-int square_solver(square_equation_data_coefficients* test);
-int linear_solver(square_equation_data_coefficients* test);
+void equation_solver(square_equation_data* test);
+void square_solver(square_equation_data* test);
+void linear_solver(square_equation_data* test);
 
-double discriminant_calculate(square_equation_data_coefficients* test);
+double discriminant_calculate(square_equation_data* test);
 
-void input_coefficients(square_equation_data_coefficients* test);
-void one_scanf(double* coefficient, const char* const output);
+void input_coefficients(square_equation_data* test);
+void input_coefficient(double* coefficient, const char* const output);
 
 void clean_buffer(void);
-int check_buffer_not_clear(void);
+bool check_buffer_not_clear(void);
 
 void finish_program(bool* more);
 
-int double_equal_comparison(double number_1, double number_2);
+bool double_equal_comparison(double number_1, double number_2);
 
-void output_roots(int root_count, square_equation_data_coefficients* test);
+void output_roots(square_equation_data* test);
 
-void test_square_solver();
+void test_square_solver(FILE* file_name);
 
-void one_test_square_solver(square_equation_data_coefficients* test_coefficients,
-                            square_equation_data_correct* test_answers);
+void one_test_square_solver(square_equation_data* test_coefficients,
+                            reference_solutions_data* test_answers);
 
-void root_sorter(square_equation_data_coefficients* test);
+void root_sorter(square_equation_data* test);
 
-const double INACCURACY = 0.0001;
+void check_file_opening(const char *file_name, FILE** file_ptr);
+void check_file_closing(FILE* file_name);
+
+const double INACCURACY = 10e-5;
 
 #endif //SQUARE_SOLVER_H
